4b0888e09ce9e67e2be19912213140ea0b1ab3f1
Avishek Das <avishek.akd@gmail.com>
2020-01-12 20:31:03 +0530

    change image name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f airflow/tiller.yaml
 
-helm upgrade --install airflow airflow/ --namespace airflow --values airflow/values.yaml
+helm upgrade --install airflow airflow/ --values airflow/values.yaml
 
-echo 192.168.99.100:`kubectl describe service airflow-web --namespace=airflow | grep NodePort | sed -n 's/.*web  \([0-9]\+\)\/TCP/\1/p' `
+echo 192.168.99.100:`kubectl describe service airflow-web | grep NodePort | sed -n 's/.*web  \([0-9]\+\)\/TCP/\1/p' `
