08117b9800250cf819a5e0e8fe5327d281712336
Avishek Das <avishek.akd@gmail.com>
2020-01-13 13:28:04 +0530

    g

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,3 +3,5 @@
 helm upgrade --install airflow airflow/ --values airflow/values.yaml
 
 echo 192.168.99.100:`kubectl describe service airflow-web | grep NodePort | sed -n 's/.*web  \([0-9]\+\)\/TCP/\1/p' `
+
+read
