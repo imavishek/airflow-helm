0469009f4ded3b935fd87a0e53a2503388281247
Avishek Das <avishek.akd@gmail.com>
2020-01-13 13:36:04 +0530

    f

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+kubectl apply -f airflow/tiller.yaml
+helm init --service-account tiller
+helm dependency update
+
 helm upgrade --install airflow airflow/ --values airflow/values.yaml
 
 echo 192.168.99.100:`kubectl describe service airflow-web | grep NodePort | sed -n 's/.*web  \([0-9]\+\)\/TCP/\1/p' `
-
-read
